9d71845522a15da08bededd9f00d8b7b847e0b79
prahaladbelavadi <github@belavadi.com>
2023-06-13 17:44:13 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CytoscapeComponent from 'react-cytoscapejs';
+
 import './App.css';
+let cytoscapeDataConfig = require('./data.json')
 
 function App() {
+  const elements = cytoscapeDataConfig.data;
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CytoscapeComponent elements={elements} style={{ width: "100vw", height: "100vh" }} />
     </div>
   );
 }
